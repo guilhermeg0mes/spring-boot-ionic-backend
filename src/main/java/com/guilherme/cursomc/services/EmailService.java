87a43ee49aa3b0434ddd98b2079281b7a9da3397
Guilherme Gomes <guilhermegomes4@hotmail.com>
2020-12-08 16:02:50 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

---
+++ src/main/java/com/guilherme/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.guilherme.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.guilherme.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+}
