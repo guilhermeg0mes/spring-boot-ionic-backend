fd10aee02762ed144119959ef76b4ada59f9a8c1
Guilherme Gomes <guilhermegomes4@hotmail.com>
2020-12-30 01:40:46 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/com/guilherme/cursomc/services/ClienteService.java
+++ src/main/java/com/guilherme/cursomc/services/ClienteService.java
@@ -14,11 +14,14 @@ import org.springframework.stereotype.Service;
 import com.guilherme.cursomc.domain.Cidade;
 import com.guilherme.cursomc.domain.Cliente;
 import com.guilherme.cursomc.domain.Endereco;
+import com.guilherme.cursomc.domain.enums.Perfil;
 import com.guilherme.cursomc.domain.enums.TipoCliente;
 import com.guilherme.cursomc.dto.ClienteDTO;
 import com.guilherme.cursomc.dto.ClienteNewDTO;
 import com.guilherme.cursomc.repositories.ClienteRepository;
 import com.guilherme.cursomc.repositories.EnderecoRepository;
+import com.guilherme.cursomc.security.UserSS;
+import com.guilherme.cursomc.services.exceptions.AuthorizationException;
 import com.guilherme.cursomc.services.exceptions.DataIntegrityException;
 import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -33,8 +36,16 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-
+	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+			
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
